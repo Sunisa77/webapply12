cbdd72112ad8dfa01a82e60f3a5063260aaf4e6b
Sunisa77 <sunisa.bn@rmuti.ac.th>
2025-10-07 09:49:25 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template,request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/predic")
+def predic_page():
+    return render_template("prediction.html")
