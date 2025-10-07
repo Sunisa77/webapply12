2ae2c77c3db73098978f489b37ae693c59115b87
Sunisa77 <sunisa.bn@rmuti.ac.th>
2025-10-07 09:54:51 +0700

    เพิ่มโค้ด

--- app.py
+++ app.py
@@ -12,6 +12,23 @@ def main():
 def about():
     return render_template("about.html")
 
-@app.route("/predic")
+@app.route("/predic", methods=["GET","POST"])
 def predic_page():
+    prediction = None
+    probability = None
+
+    if request.method == "POST":
+        temperature = float(request.form.get("temperature"))
+        humidity = float(request.form.get("humidity"))
+        water_level = float(request.form.get("water_level"))
+        N = float(request.form.get("N"))
+        P = float(request.form.get("P"))
+        K = float(request.form.get("K"))
+
+        input_data = np.array([[temperature, humidity, water_level, N, P, K]])
+        prediction = model.predict(input_data)[0]
+        probability = model.predict_proba(input_data).max()
+
+        prediction = "เปิด" if prediction == 1 else "ปิด"
+        probability = f"{probability * 100:.2f}%"
     return render_template("prediction.html")
